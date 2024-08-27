fbf2e0aaf12a19062f780c4eef24367bda753406
Manases Lovera <manaseslovera07@gmail.com>
2024-08-27 16:47:46 -0400

    problem 1 solved using python, that way I'm able to not use ternary operators

---
+++ src/1.convert_rgb_hsv/app.py
@@ -0,0 +1,46 @@
+import math
+def rgbToHsv(r, g, b):
+    r /= 255
+    g /= 255
+    b /= 255
+
+    cMax = max(r, g, b)
+    cMin = min(r, g, b)
+    chroma = cMax - cMin
+
+    h = 0
+    h = ((g - b) / chroma) * (cMax == r)
+    h = ((b - r) / chroma + 2) * (cMax == g)
+    h = ((r - g) / chroma + 4) * (cMax == b)
+
+    # If all RGB are zero this will stop execution, just trying not to use
+    # any conditional statement
+    S = chroma / cMax
+    V = cMax
+
+    return [int(h), int(S), int(V)]
+
+def hsvToRgb(h, s, v):
+    h = h % 360
+
+    C = v * s
+    X = C * (1 - abs((h / 60) % 2 - 1))
+    M = v - C
+
+    HIndex = math.floor(h / 60) % 6
+
+    R = 0 + C * (HIndex == 0 or HIndex == 5)
+    R += X * (HIndex == 1 or HIndex == 4)
+    G = 0 + C * (HIndex == 1 or HIndex == 2)
+    G += X * (HIndex == 0 or HIndex == 3)
+    B = 0 + C * (HIndex == 2 or HIndex == 3)
+    B += X * (HIndex == 4 or HIndex == 5)
+
+    return [
+        round((R + M) * 255),
+        round((G + M) * 255),
+        round((B + M) * 255)
+    ]
+
+print(rgbToHsv(255,0,0)) # Returns [0, 1, 1]
+print(hsvToRgb(0, 1, 1)) # Returns [255, 0, 0]
