f05c8f965e40ed32189f6642ecf0b6c11c7cee1b
Manases Lovera <manaseslovera07@gmail.com>
2024-08-28 00:37:59 -0400

    message for problem 4 completed

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Tagshelf Interview Test
 
-### Convert RGB to HSV and HSV to RGB using typescript
+### 1. Convert RGB to HSV and HSV to RGB using typescript
 
 For running this code you must have node install, I'm using v20.10.0 <br>
 I used typescript for this problem, if you want to executed with an example, first do `npm install`, and then `npm run rgb_hsv`. <br>
 I also used python then because the problem said I should've not use any conditionals, didn't find a way to do it without them using typescript. To compile the python program you must use `py <filename>` or just click on the button in vscode, must have python 3 installed.
 
-### Return value with coordenates using C++
+### 2. Return value with coordenates using C++
 
 For running this code I followed the [visual studio docs](https://code.visualstudio.com/docs/cpp/config-mingw) steps to configure the editor to use the GCC C++ compiler (g++) from mingw-w64. <br>>
 First I wanted to use one programming language, but then decided to use different as a personal challenge, to run this code you can either click on F5, click on run using the vscode interface
+
+### 3. Cound Word
+
+### 4. Identify language using C#
+
+I decided building an API using ASP .NET Core, since it was just an endpoint I did a minimal api, to run this code you must have .net 8 installed and in the right directory just type `dotnet run` and that's all, it will open a zwagger app.
--- src/4.identify_language/Program.cs
+++ src/4.identify_language/Program.cs
@@ -3,14 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -19,6 +16,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+/*
+Ok the approach I took for this problem was very easy, the largest the text and accuracy is better, since the dictionary is not that long,
+in a real life task I would use an api or ai tool for this to get the best results, but since I'm not able to do that I just created two 
+dictionaries for english and spanish, I searhed the most common words in both languages, if I found only for english, the response will be
+english and the same for spanish, if I found for both I will send spanglish, and if it doesn't math I will send an error message.
+*/
+
 var englishDictionary = new[] {
     "the ", "and ", " is ", " in", "hello", "how", "what", "where", "when", "thanks", 
     " be", "that", " for", "you", "string", "number", " of ", " but ", "would", "their",
@@ -26,8 +30,9 @@ var englishDictionary = new[] {
 };
 
 var spanishDictionary = new[] {
-    "el ", " y ", " es ", " en ", "mañana", "hola", "ñ", "á", "é", "í", "ó", "ú", "para", 
-    " del", " las ", " los ", "texto", "numero", "¿", "porque ", "aunque", "siempre", "¡"
+    " el ", " y ", " es ", " en ", "mañana", "hola", "ñ", "á", "é", "í", "ó", "ú", "para", 
+    " del ", " las ", " los ", "texto", "numero", "¿", "porque ", "aunque", "siempre", "¡",
+    " la "
 };
 
 app.MapPost("/detect-language", ([FromBody] RequestTextDto requestTextDto) =>
